4524c5f8eb12c76df3ce6fb513eaab0bc353d6f6
Imagine <imaginejhy@163.com>
2023-10-05 00:13:13 +0800

    update .gitignore

--- mit6824_lab1/build/CMakeFiles/imagine_mapreduce_make.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/imagine_mapreduce_make"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imagine_mapreduce_make.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- mit6824_lab1/build/CMakeFiles/init.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/init"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/init.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
